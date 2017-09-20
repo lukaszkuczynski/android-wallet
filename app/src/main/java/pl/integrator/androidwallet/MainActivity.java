71915b8da4e58be6eaf89387530bc35f36802828
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-09-20 19:43:16 +0200

    Simple view with elements created

--- app/src/main/java/pl/integrator/androidwallet/MainActivity.java
+++ app/src/main/java/pl/integrator/androidwallet/MainActivity.java
@@ -1,45 +1,26 @@
 package pl.integrator.androidwallet;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button button = (Button) findViewById(R.id.save);
+
+        button.setOnClickListener( (View view) -> {
+            Snackbar.make(view, "You clicked me, Wallet Navigator!", Snackbar.LENGTH_INDEFINITE)
+                    .show();
+        });
+
     }
 
 }
