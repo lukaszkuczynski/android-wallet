51a5e7042ea35e70e15bf36a2f3ae540a4ba60c8
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-09-20 21:25:45 +0200

    Spinner replaces Textedit for operations tagging

--- app/src/main/java/pl/integrator/androidwallet/MainActivity.java
+++ app/src/main/java/pl/integrator/androidwallet/MainActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +25,32 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.save);
 
         final EditText amountEdit = (EditText) findViewById(R.id.amount);
-        final EditText descriptionEdit = (EditText) findViewById(R.id.description);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        String[] tags = new String[]{
+                "shopping",
+                "gasoline"
+        };
+
+        final List<String> tagList = new ArrayList<>(Arrays.asList(tags));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(
+                this,R.layout.support_simple_spinner_dropdown_item,tagList);
+
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+
 
         button.setOnClickListener( (View view) -> {
 
             double amount = Double.valueOf(amountEdit.getText().toString());
-            String description = descriptionEdit.getText().toString();
-            String snackText = String.format("Adding operation '%s' with amount of %.2f", description, amount);
+            String tag = spinner.getSelectedItem().toString();
+            String snackText = String.format("Adding operation '%s' with amount of %.2f", tag, amount);
             Snackbar.make(view, snackText, Snackbar.LENGTH_INDEFINITE)
                     .show();
-            operationDao.saveOperation(new Operation(amount, description));
+            operationDao.saveOperation(new Operation(amount, tag));
         });
 
     }
